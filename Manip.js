a1d5c5decd479e959506b8abf09769717111de8b
athanase bureau <athanasey376@gmail.com>
2025-06-19 18:14:00 +0000

    vidange page ameliore, parfait

--- Manip.js
+++ Manip.js
@@ -10,16 +10,8 @@ function vd_pgweb(){
 
 for (var i= 0; i < 11; i++) 
 {
-if (chpi_vd[i].value.trim()===""||chpi_vd[i].value.trim () ==="null"||chpi_vd[i].value.trim()==="indefined"){lgn_pt[i+1].remove();i--;}
+if (lgn_pt[i].firstElementChild.value.trim() ===""||lgn_pt[i].firstElementChild.value.trim() ==="null"||lgn_pt[i].firstElementChild.value.trim() ==="indefined"){lgn_pt[i].remove();i--;}
 }
 }
 
-/*var att_n = document.getElementById("nom");
-var att_pr = document.getElementById("prenom");
-var att_pr_rb = document.getElementById("Label2");
-var parent_att_pr=document.getElementsByTagName("div");
-;
-
-function vd_pgweb (){
-if (att_pr.value===""){parent_att_pr[1].removeChild(att_pr_rb);}
-}*/
+/*alert(lgn_pt[0].firstElementChild.value);*/
