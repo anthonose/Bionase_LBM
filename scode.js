73a3134a4cbf48050aee974327f40122b5677cea
anthonose <athanasey376@gmail.com>
2026-01-29 17:42:26 +0000

    Add files via upload

--- scode.js
+++ scode.js
@@ -1,1 +1,8 @@
 export let exaGrp={_id:"696a6f4fef373f339824a88a",nom:"keita",prenoms:"foussenan",Sexe:"F",Telephone:"2510365237",DiagnosticClinique:"fièvre noctune",Prescripteur:"dr konate",Biotechnologiste:"amani",PrixExamens:"assure",DateduPrelevement:"2026-01-16",t3l:"on",t4l:"on",tck:"on",TCK:"45",T3_libre:"6",T4_libre:"6",bHCG_plasma:"20",bHCG_Uri:"31",};
+
+let exaGrp={_id:"696a6f4fef373f339824a88a",nom:"keita",prenoms:"foussenan",Sexe:"F",Telephone:"2510365237",DiagnosticClinique:"fièvre noctune",Prescripteur:"dr konate",Biotechnologiste:"amani",PrixExamens:"assure",DateduPrelevement:"2026-01-16",t3l:"on",t4l:"on",tck:"on",TCK:"45",T3_libre:"6",T4_libre:"6",bHCG_plasma:"20",bHCG_Uri:"31",};
+
+let exaGrp={_id:"696a6f4fef373f339824a88a",nom:"keita",prenoms:"foussenan",Sexe:"F",Telephone:"2510365237",DiagnosticClinique:"fièvre noctune",Prescripteur:"dr konate",Biotechnologiste:"amani",PrixExamens:"assure",DateduPrelevement:"2026-01-16",t3l:"on",t4l:"on",tck:"on",
+Uree:"0.20",Lipide_Tot:"100",Acide_Urique:"50.31",Creatinine:"14.25",Glycemie:"0.96",
+Sodium:"131",Potassium:"3.9",Chlore:"103",TGO:"22",TGP:"35",Triglycerides:"1.2",Cholesterol_Tot:"1.85",Cholesterol_HDL:"0.75",Cholesterol_LDL:"0,25",
+TCK:"45",T3_libre:"6",T4_libre:"6",bHCG_plasma:"20",bHCG_Uri:"31",};
