2f6f909dc87e156df1dd1507f67b6b72c27abd37
anthonose <athanasey376@gmail.com>
2026-02-09 17:52:25 +0000

    Add files via upload

--- scode.js
+++ scode.js
@@ -4,7 +4,7 @@ let exaGrp={_id:"696a6f4fef373f339824a88a",nom:"keita",prenoms:"foussenan",Sexe:
 
 let exaGrp={_id:"696a6f4fef373f339824a88a",nom:"keita",prenoms:"foussenan",Sexe:"F",Telephone:"2510365237",DiagnosticClinique:"fièvre noctune",Prescripteur:"dr konate",Biotechnologiste:"amani",PrixExamens:"assure",DateduPrelevement:"2026-01-16",t3l:"on",t4l:"on",tck:"on",
 Uree:"0.20",Lipide_Tot:"100",Acide_Urique:"50.31",Creatinine:"14.25",Glycemie:"0.96",Calcium:"96",Magnesium:"29.6",Protides_Tot:"10.9",PhosphataseAlcaline:"200.16",GAMMA_GT:"15.6",Bilirubine_Tot:"11.96",Bilirubine_Conj:"1.96",Glycosurie :"0.32",Albuminurie :"0.16",Ferritine:"70",
-Sodium:"131",Potassium:"3.9",Chlore:"103",TGO:"22",TGP:"35",Triglycerides:"1.2",Cholesterol_Tot:"1.85",Cholesterol_HDL:"0.75",Cholesterol_LDL:"0,25",HBA1C:"8.23",
+Sodium:"131",Potassium:"3.9",Chlore:"103",TGO:"22",TGP:"35",Triglycerides:"1.2",Cholesterol_Tot:"1.85",Cholesterol_HDL:"0.75",Cholesterol_LDL:"0,25",HBA1C:"8.23",Toxoplasmose:"NEGATIVE",toxoIgm:"91",toxoIgg:"21",Bilharziose:"NEGATIVE",bilhIgm:"85",bilhIgg:"44",BW:"NEGATIVE",tpha:"1026",ASLO:"NEGATIVE",asloT:"400",Chlamydia:"NEGATIVE",chldIgm:"78",Waaler_Rose:"NEGATIVE",wlrT:"65",Rubeole:"NEGATIVE",rubIgm:"125",chldIgg:"102",VIH:"NEGATIVE",vihTp:"VIH1",
 Goutte_Epaisse:"NEGATIVE",tGE:"123",SWidal_Felix:"NEGATIVE",BH:"123",CO:"123",CH:"123",TO:"123",TH:"123",AO:"123",AH:"123",BO:"123",CRP:"NEGATIVE",crpT:"123",
 nfsLeucocytes:"7",nfsHematies:"4.02",nfsHemoglobine:"12.06",nfsHematocrite:"36.18",nfsVGM:"80",nfsTCMH:"27",nfsCCMH:"29",nfsPlaquette:"213",nfsPN:"35",nfsPE:"2",nfsPB:"0",nfsLY:"59",nfsMn:"4",
 TCK:"45",T3_libre:"6",T4_libre:"6",bHCG_plasma:"20",bHCG_Uri:"31",};
